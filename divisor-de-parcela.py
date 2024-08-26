63148cad6eea1e6b87ab44ebfd70a81e7c99c81a
cefasah <dropeapelao64@gmail.com>
2024-08-25 23:21:44 -0300

    Create divisor-de-parcela.py

---
+++ divisor-de-parcela.py
@@ -0,0 +1,25 @@
+def valor (x ) :
+   
+   return 
+   print("escreva quanto vc quer receber de emprestimo")
+   x = int(input( valor ) )
+ 
+def parcela ( y ): 
+   return
+   print ("quantas parcelas vc quer? ") 
+   y = int(input( parcela ) ) 
+              
+
+def R ( resultado):
+   return resultado
+
+
+print("escreva quanto vc quer receber de emprestimo")
+x = int(input( valor ) )
+print ("quantas parcelas vc quer? ")
+y = int(input( parcela ) ) 
+              
+resultado = ( x / y)
+
+print (f" o valor pago men mensalemnte é de {resultado} ") 
+
